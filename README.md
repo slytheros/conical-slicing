539fbab3470e45345afe97601721e100feb6b827
slytheros <cevripidou87@gmail.com>
2023-12-23 23:32:40 +0200

    Added docstrings, some extra logging and tidied up CLI argument parsing

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# conical-slicing
+# Introduction
+Reimplementation of the slicing strategy introduced by [Wüthrich et al.](https://www.researchgate.net/publication/354726760_A_Novel_Slicing_Strategy_to_Print_Overhangs_without_Support_Material)
+
+
+
+```bibtex
+@Article{app11188760,
+AUTHOR = {Wüthrich, Michael and Gubser, Maurus and Elspass, Wilfried J. and Jaeger, Christian},
+TITLE = {A Novel Slicing Strategy to Print Overhangs without Support Material},
+JOURNAL = {Applied Sciences},
+VOLUME = {11},
+YEAR = {2021},
+NUMBER = {18},
+ARTICLE-NUMBER = {8760},
+URL = {https://www.mdpi.com/2076-3417/11/18/8760},
+ISSN = {2076-3417},
+ABSTRACT = {Fused deposition modeling (FDM) 3D printers commonly need support material to print overhangs. A previously developed 4-axis printing process based on an orthogonal kinematic, an additional rotational axis around the z-axis and a 45° tilted nozzle can print overhangs up to 100° without support material. With this approach, the layers are in a conical shape and no longer parallel to the printing plane; therefore, a new slicer strategy is necessary to generate the paths. This paper describes a slicing algorithm compatible with this 4-axis printing kinematics. The presented slicing strategy is a combination of a geometrical transformation with a conventional slicing software and has three basic steps: Transformation of the geometry in the .STL file, path generation with a conventional slicer and back transformation of the G-code. A comparison of conventionally manufactured parts and parts produced with the new process shows the feasibility and initial results in terms of surface quality and dimensional accuracy.},
+DOI = {10.3390/app11188760}
+}
+```
+# Getting Started
+
+You will need:
+- PrusaSlicer (currently this is the only supported slicer).
+- Python with the packages listed in `requirements.txt`:
+  - vtkplotlib~=2.1.0
+  - tqdm~=4.66.1
+  - numpy~=1.26.2
+
+The script should work cross-platform but was only tested on Windows.
+
+
+## Setup Python
+To setup your python environment you can either set up a virtual environment 
+(my personal preference), or use your system interpreter:
+
+To use a virtual environment:
+1. Create a virtual environment `python -m venv venv` (the second `venv` is the name of the environment; you can choose your own)
+2. Activate the virtual environment (`venv/Scripts/activate`).
+3. Install the required python modules: `python -m pip install -r requirements.txt`
+4. Make sure everything is OK by calling `python conical_slice.py --help`. If all modules are installed, you should see the help for the CLI.
+
+To use your system python, do steps 3 and 4.
+
+# How to use the script
+
+First things first, if you're using a venv, make sure it's activated, or you use the python executable in your `<venv>` 
+folder (usually `Scripts/python.exe`).
+
+You will also need a PrusaSlicer Config file. To do this, open PrusaSlicer, select your printer, filament and layer height as normal. Then export by File > Export > Export Config (or CTRL+E).
+
+To see the available options, call `python conical_slice.py --help`; that's the source of truth for the command line interface.
+
+Example usage:
+
+```shell
+python conical_slice.py \
+  -i tree.stl \
+  -o my_gcode.gcode \
+  --prusa-slicer-config config.ini \
+  --cone-origin 0 0 \
+  --angle 10 \
+  --preview
+```
--- conical_slice.py
+++ conical_slice.py
@@ -6,23 +6,35 @@ import subprocess
 import vtkplotlib
 import tqdm
 import numpy
-import time
 import logging
 
 from collections import OrderedDict
 from stl import mesh
 
+logging.basicConfig(filename='conical_slice.log')
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 
 def load_mesh(filename: str) -> mesh.Mesh:
+    """
+    Load a mesh from file and return it as a mesh.Mesh.
+
+    :param filename: path to the STL file
+    :return: a mesh.Mesh
+    """
     logger.info(f"Loading mesh from {filename}")
     np_mesh = mesh.Mesh.from_file(filename)
     return np_mesh
 
 
 def subdivide_triangle(triangle_points: numpy.ndarray) -> numpy.ndarray:
+    """
+    Subdivide a triangle in 3D space into 4 sub-triangles using the mid-points of all edges.
+
+    :param triangle_points:
+    :return: a numpy.ndarray containing the subdivided triangles
+    """
     triangles = numpy.zeros((4, 3, 3))
     point1 = triangle_points[0]
     point2 = triangle_points[1]
@@ -39,6 +51,16 @@ def subdivide_triangle(triangle_points: numpy.ndarray) -> numpy.ndarray:
 
 
 def subdivide(my_mesh: mesh.Mesh) -> mesh.Mesh:
+    """
+    Increases the polygon count of a mesh by subdividing each triangle into 4 sub-triangles.
+    Maintains geometry (no interpolation)
+
+    TODO: This is called iteratively and its execution time is increased exponentially with each iteration.
+    TODO: Might be worth re-implementing to do selective subdivision for triangles with edges under a threshold
+
+    :param my_mesh: the mesh to subdivide
+    :return: the subdivided mesh
+    """
     logger.info(f"Subdividing mesh")
     divided_vectors = numpy.zeros(shape=(my_mesh.vectors.shape[0] * 4, 3, 3))
     for ix, vector in tqdm.tqdm(enumerate(my_mesh.vectors), desc="Subdividing mesh"):
@@ -55,6 +77,11 @@ def subdivide(my_mesh: mesh.Mesh) -> mesh.Mesh:
 
 
 def reset_origin(np_mesh: mesh.Mesh) -> mesh.Mesh:
+    """
+    Resets the origin of the mesh. The origin is set to the center of the object.
+    :param np_mesh:
+    :return:
+    """
     x_delta = - np_mesh.x.min() - (np_mesh.x.max() - np_mesh.x.min()) / 2
     y_delta = - np_mesh.y.min() - (np_mesh.y.max() - np_mesh.y.min()) / 2
 
@@ -68,6 +95,13 @@ def reset_origin(np_mesh: mesh.Mesh) -> mesh.Mesh:
 
 
 def stl_transform(np_mesh: mesh.Mesh, angle_degrees: int | float, cone_origin: numpy.array) -> mesh.Mesh:
+    """
+    Transforms the mesh by offsetting the z axis using by a cone transformation.
+    :param np_mesh:
+    :param angle_degrees:
+    :param cone_origin: the position of the cone
+    :return:
+    """
     np_mesh.z += (np_mesh.z > 0.1) * numpy.sqrt(
         (np_mesh.x - cone_origin[0]) ** 2 + (np_mesh.y - cone_origin[1]) ** 2) * numpy.sin(
         numpy.radians(angle_degrees))
@@ -77,6 +111,13 @@ def stl_transform(np_mesh: mesh.Mesh, angle_degrees: int | float, cone_origin: n
 
 
 def plot(np_mesh: mesh.Mesh, angle_degrees: int | float, cone_origin: numpy.array) -> None:
+    """
+    Shows a preview of a mesh and an illustration of the cone location and angle.
+    :param np_mesh:
+    :param angle_degrees:
+    :param cone_origin:
+    :return:
+    """
     logging.info(f"Rendering mesh with {angle_degrees=} and {cone_origin=}")
     mesh_plot = vtkplotlib.mesh_plot(np_mesh)
     vtkplotlib.plot(
@@ -84,39 +125,43 @@ def plot(np_mesh: mesh.Mesh, angle_degrees: int | float, cone_origin: numpy.arra
                      [cone_origin[0], cone_origin[1], np_mesh.z.max() * 1.1]]),
         color='r',
         fig=mesh_plot.fig)
-    vtkplotlib.plot(
-        numpy.array([[cone_origin[0], cone_origin[1], 0],
-                     [0, 0, np_mesh.z.max() * 1.1 * numpy.sin(numpy.radians(angle_degrees))]]),
-        color='b',
-        fig=mesh_plot.fig)
-    vtkplotlib.show(block=False, fig=mesh_plot.fig)
-
-
-def get_bed_center(config_file: str) -> numpy.array:
-    logging.info("Reading config file to get bed center")
-    with open(config_file) as f:
-        while True:
-            line = f.readline()
-            if not line:
-                break
-            if line.strip().startswith('bed_shape'):
-                bed_shape = numpy.array([list(map(float, x.split('x'))) for x in line.split('=')[1].strip().split(',')])
-                bed_center = (bed_shape[:, 0].max() - bed_shape[:, 0].min()) / 2, (
-                        bed_shape[:, 1].max() - bed_shape[:, 1].min()) / 2
-                logging.info(f"Bed center is {bed_center}")
-                return bed_center
+    for i in range(10):
+        vtkplotlib.plot(
+            numpy.array([[cone_origin[0], cone_origin[1], i * np_mesh.z.max() / 10],
+                         [cone_origin[0] + np_mesh.x.max()* 1.5, cone_origin[1],
+                          i * np_mesh.z.max() / 10 + np_mesh.x.max()* 1.5 * numpy.sin(numpy.radians(angle_degrees))]]),
+            color='b',
+            fig=mesh_plot.fig)
+    vtkplotlib.show(block=True, fig=mesh_plot.fig)
 
 
 def get_distance(point1: tuple, point2: tuple) -> float:
+    """
+    Calculates the distance between two points in 3D space.
+    :param point1:
+    :param point2:
+    :return:
+    """
     distance = numpy.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2 + (point1[2] - point2[2]) ** 2)
     return distance
 
 
 def parse_g_gcode(gcode_line: str) -> dict[str:[None | float]]:
-    gcode_dict = OrderedDict([('G', None), ('X', None), ('Y', None), ('Z', None), ('E', None), ('F', None)])
+    """
+    Takes a G command and parses it into a dictionary.
 
+    :param gcode_line:
+    :return: an ordered dictionary with the components of the g command. Missing components are set to None.
+    """
+    if not (gcode_line.strip().startswith('G0') or gcode_line.strip().startswith('G1')):
+        msg = f'Invalid command while parsing {gcode_line.strip()}'
+        logging.error(msg)
+        raise Exception(msg)
+
+    gcode_dict = OrderedDict([('G', None), ('X', None), ('Y', None), ('Z', None), ('E', None), ('F', None)])
     reading = None
     num_str = str()
+
     for c in gcode_line:
         if c == ';':
             break
@@ -130,25 +175,44 @@ def parse_g_gcode(gcode_line: str) -> dict[str:[None | float]]:
             gcode_dict[reading] = float(num_str)
     if reading:
         gcode_dict[reading] = float(num_str)
+
+    logging.debug(f"Parsed {gcode_line.strip()} as {gcode_dict}.")
     return gcode_dict
 
 
-def g_cmd_to_str(gcode_dict: dict) -> str:
+def g_cmd_to_str(gcode_dict: dict[str:[None | float | int]]) -> str:
+    """
+    Converts a g command dictionary into G code.
+    :param gcode_dict:
+    :return:
+    """
     gcode_str = ''
     for k, v in gcode_dict.items():
         if k == 'G':
             gcode_str += f"{k}{int(v)} "
         elif v is not None:
             gcode_str += f"{k}{v:.7g} "
+    logging.debug(f"Converted {gcode_dict=} into {gcode_str=}.")
     return gcode_str
 
 
 def transform_gcode_line(gcommand: dict,
                          bed_center: tuple,
                          angle_degrees: int | float,
-                         previous_location: tuple = (0, 0, 0),
-                         first_layer_height: float = 0.2,
-                         cone_origin: numpy.ndarray = numpy.array([0., 0.])) -> dict:
+                         previous_location: tuple,
+                         first_layer_height: float,
+                         cone_origin: numpy.ndarray) -> dict:
+    """
+    Back-transforms the G command to remove the conic transformation offset that was applied to the original STL file.
+
+    :param gcommand:
+    :param bed_center:
+    :param angle_degrees:
+    :param previous_location:
+    :param first_layer_height:
+    :param cone_origin:
+    :return:
+    """
     gcode_dict = gcommand.copy()
 
     if gcode_dict['X'] is not None or gcode_dict['Y'] is not None or gcode_dict['Z'] is not None:
@@ -170,7 +234,7 @@ def transform_gcode_line(gcommand: dict,
         # adapt extrusion length for new travel distance.
         if (gcode_dict['E'] is not None) and (travel_distance > 0):
             gcode_dict['E'] = gcode_dict['E'] * get_distance((tmp_x, tmp_y, tmp_z), previous_location) / travel_distance
-
+    logging.debug(f"Converted {gcommand=} into {gcode_dict=} when {previous_location=}.")
     return gcode_dict
 
 
@@ -182,6 +246,11 @@ def cast(type, value):
 
 
 def parse_config(config_file: str) -> dict[str:[str | float | int]]:
+    """
+    Parses the config file exported by PrusaSlicer and returns it as a dictionary.
+    :param config_file:
+    :return:
+    """
     config = dict()
     with open(config_file) as fh:
         lines = fh.readlines()
@@ -205,12 +274,20 @@ def parse_config(config_file: str) -> dict[str:[str | float | int]]:
                 config['bed_center'] = bed_center
             else:
                 config[l[0].strip()] = value if value is not None else l[1]
+            logger.debug(f'Config property found: {line.strip()} ==> {l[0].strip()}:{config[l[0].strip()]}')
     return config
 
 
 def get_nozzle_location(move_cmd: dict[str:[int | float]] | str,
                         nozzle_location: tuple[[int | float], [int | float], [int | float]]
                         ) -> tuple[[int | float], [int | float], [int | float]]:
+    """
+    Utility function to get the location of the nozzle after a G command is executed.
+
+    :param move_cmd: the G command
+    :param nozzle_location: the location of the nozzle before the g command is executed
+    :return:
+    """
     if type(move_cmd) == str:
         move_dict = parse_g_gcode(move_cmd)
     else:
@@ -260,11 +337,12 @@ def gcode_transform(gcode_file: str,
             continue
         else:
             parsed_g_command = parse_g_gcode(line)
-            nozzle_location = get_nozzle_location(move_cmd=move_cmd, nozzle_location=nozzle_location)
+            nozzle_location = get_nozzle_location(move_cmd=parsed_g_command, nozzle_location=nozzle_location)
             move_cmd = transform_gcode_line(parsed_g_command,
                                             bed_center=config['bed_center'],
                                             angle_degrees=angle_degrees,
                                             previous_location=nozzle_location,
+                                            first_layer_height=config['first_layer_height'],
                                             cone_origin=cone_origin)
 
             # If we're bridging add some minor fuzzing to make sure it sticks to the model
@@ -280,57 +358,98 @@ def gcode_transform(gcode_file: str,
     return transformed_gcode
 
 
-def slice_stl(stl_file: str, prusaslicer_path: str, config_file: str, gcode_output: str):
+def slice_stl(stl_file: str, prusaslicer_path: str, config_file: str, gcode_output: str) -> None:
+    """
+    Slices the stl file using PrusaSlicer and the config profile provided.
+
+    :param stl_file: the STL file to be sliced
+    :param prusaslicer_path: path to `prusa-slicer-console` executable
+    :param config_file: the config file used during slicing
+    :param gcode_output: the path of the gcode output file
+    """
+
     command = [
         prusaslicer_path,
         '-g', stl_file,
         '--load', config_file,
-        '--skirts', '0',
+        '--skirts', '0',  # TODO: make the post-processor clever enough so that we can have a skirt.
         '--output', gcode_output,
         '--infill-overlap', '50%'
     ]
-
+    logger.info(f"Slicing {stl_file}: {' '.join(command)}")
     result = subprocess.run(command)
     if result.returncode != 0:
         raise Exception("Slicing failed.")
 
 
-if __name__ == '__main__':
+def setup_cli_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input', type=str, required=True, help='Input STL file')
     parser.add_argument('-o', '--output', type=str, help='Output gcode file')
     parser.add_argument('--prusa-slicer-path', type=str,
                         default=r'C:\Program Files\Prusa3D\PrusaSlicer\prusa-slicer-console.exe',
                         help='Path to prusa-slicer-console.exe')
-    parser.add_argument('--prusa-slicer-config', default='config.ini', type=str)
-    parser.add_argument('--cone-origin', type=float, nargs=2, default=[0., 100.],
+    parser.add_argument('--prusa-slicer-config',
+                        help="Path to PrusaSlicer config file. Hint: File > Export > Export Config (or CTRL+E).",
+                        type=str)
+    parser.add_argument('--cone-origin',
+                        type=float,
+                        nargs=2,
+                        default=[0., 0.],
                         help="Origin of the cone, offset from the center of the object.")
-    parser.add_argument('--angle', type=float, default=10.0, help='Angle in degrees')
+    parser.add_argument('--angle',
+                        type=float,
+                        default=10.0,
+                        help='Cone angle in degrees.')
+    parser.add_argument('--subdivide-threshold',
+                        type=float,
+                        default=5.0,
+                        help='The minimum acceptable distance between any edges in mm. Used during iterative subdivision')
+    parser.add_argument('--max-subdivision-iterations',
+                        type=int,
+                        default=5,
+                        help='The maximum number of iterations to run the subdivision algorithm.')
+    parser.add_argument('--preview',
+                        action='store_true',
+                        default=False,
+                        help='Preview the transformed mesh')
+    return parser.parse_args()
 
-    args = parser.parse_args()
+
+if __name__ == '__main__':
+    args = setup_cli_args()
 
     if not os.path.isfile(args.input):
-        raise FileNotFoundError('Input file does not exist')
+        raise FileNotFoundError('Input file does not exist.')
 
+    # Load the mesh and make sure it's center is set to (0,0)
     mesh_obj = load_mesh(args.input)
     mesh_obj = reset_origin(mesh_obj)
+
+    # Iteratively subdivide the mesh so that there are no long lines
     i = 0
-    while (max_vector := max(numpy.sqrt(numpy.sum(numpy.power(mesh_obj.v0 - mesh_obj.v1, 2), axis=1)))) > 5:
+    while (max_vector := max(
+            numpy.sqrt(numpy.sum(numpy.power(mesh_obj.v0 - mesh_obj.v1, 2), axis=1)))) > args.subdivide_threshold:
         i += 1
         logging.info('Subdividing. Max vector length: {:.2f}.'.format(max_vector))
-        if i > 5:
-            logging.warning("Max iterations for subdivide reached.")
+        if i > args.max_subdivision_iterations:
+            logging.warning(f"Max iterations for subdivide reached but the maximum vector length is {max_vector}mm.")
             break
-        start = time.time_ns()
         mesh_obj = subdivide(mesh_obj)
-        print(i, (time.time_ns() - start) / 1000000)
     mesh_obj = stl_transform(mesh_obj, angle_degrees=args.angle, cone_origin=numpy.array(args.cone_origin))
-    plot(np_mesh=mesh_obj, angle_degrees=args.angle, cone_origin=numpy.array(args.cone_origin))
-    transformed_stl_name = pathlib.Path(args.input).stem + '.transformed.stl'
+    if args.preview:
+        plot(np_mesh=mesh_obj, angle_degrees=args.angle, cone_origin=numpy.array(args.cone_origin))
 
+    # TODO: use temporary files or cleanup
+    transformed_stl_name = pathlib.Path(args.input).stem + '.transformed.stl'
     intermediate_gcode_name = pathlib.Path(args.input).stem + '.temp.gcode'
+
     mesh_obj.save(transformed_stl_name)
     slice_stl(transformed_stl_name, args.prusa_slicer_path, args.prusa_slicer_config, intermediate_gcode_name)
     with open(args.output, 'w') as out_f:
-        out_f.write('\n'.join(gcode_transform(gcode_file=intermediate_gcode_name, config_file=args.prusa_slicer_config,
-                                              angle_degrees=args.angle, cone_origin=numpy.array(args.cone_origin))))
+        out_f.write('\n'.join(gcode_transform(gcode_file=intermediate_gcode_name,
+                                              config_file=args.prusa_slicer_config,
+                                              angle_degrees=args.angle,
+                                              cone_origin=numpy.array(args.cone_origin))
+                              )
+                    )
